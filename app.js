e28545d5851b8fbf58b856a6a383bb32738b11bb
Arjun <arjunsingh8112@gmail.com>
2022-07-27 11:10:20 +0530

    prevent making moves on top of previous ones

--- app.js
+++ app.js
@@ -11,11 +11,9 @@ const gameBoard = (() => {
         const boardCell = (() => {
             const cell = document.createElement('div')
             cell.classList.add('cell');
-            cell.textContent = board[i]
             document.querySelector('.board').appendChild(cell)
         })();
     }
-
     (function addMarks() {
         let currentMove = 0;
         const cellArr = document.querySelectorAll('.cell')
@@ -23,29 +21,27 @@ const gameBoard = (() => {
             const markChanger = ((cell) => {
                 cell.addEventListener('click',
                     function () {
-                        currentMove % 2 === 0 ? this.textContent = 'x' : this.textContent = 'o'
-                        console.log(this)
-                        console.log('BRUHHHHH');
-                        currentMove++;
+                        if (this.textContent == '') {
+                            currentMove % 2 === 0 ? this.textContent = 'x' : this.textContent = 'o'
+                            currentMove++;
+                        }
+                        else {
+                            alert('That move is invalid')
+                        }
+
                     })
             });
-
-
             for (let i = 0; i < cellArr.length; i++) {
                 markChanger(cellArr[i])
             }
-
-
         })();
         return {
             addXMark
         }
     })();
-
     return {
         board
     }
-
 })();
 
 const displayController = (() => {
@@ -54,13 +50,3 @@ const displayController = (() => {
 
     }
 })();
-
-
-
-
-
-// Build the functions that allow players to add marks to a specific spot on the board, 
-// and then tie it to the DOM, letting players click on the gameboard to place their marker.
-// Don’t forget the logic that keeps players from playing in spots that are already taken!
-
-// Think carefully about where each bit of logic should reside.Each little piece of functionality should be able to fit in the game, player or gameboard objects..but take care to put them in “logical” places.Spending a little time brainstorming here can make your life much easier later!
