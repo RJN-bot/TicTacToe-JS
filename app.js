5a0e6825a13d7b393adb7e0725fefcdf013cd6f1
Arjun <arjunsingh8112@gmail.com>
2022-07-28 14:24:20 +0530

    Still trying, no idea how to

--- app.js
+++ app.js
@@ -45,15 +45,34 @@ const gameBoard = (() => {
             const winningCombos = [
                 [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
             ]
-            for (let i = 0; i < cellArr.length; i++) {
-                console.log(cellArr[i].textContent)
+            let winningCombosEachArr = (() => {
+                for (let i = 0; i < winningCombos.length; i++) {
+                    winningCombos[i]
+                }
+            })()
+
+
+            let markArr = []
+            const markPositionGetter = (() => {
+                for (let i = 0; i < cellArr.length; i++) {
+                    markArr.push(cellArr[i].textContent)
+
+                }
+                // const positionOfMark = () => {
+                // }
+                console.log(markArr)
+
+            })()
+
+            if (markArr.includes(winningCombosEachArr)) {
+                alert('bhbksnkasj')
             }
 
         })
 
-        while (1 = 3) {
+        // while (1 = 3) {
 
-        }
+        // }
 
 
         return {
